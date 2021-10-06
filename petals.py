503882334bb01a2849cbcda8f8b73ace28841987
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-10-06 13:04:25 +1030

    add description and instruction

--- petals.py
+++ petals.py
@@ -1,2 +1,27 @@
+#!/usr/bin/env python3
+
+#
+# File:         petals.py
+# Author:       Tan Duc Mai
+# Email:        tan.duc.work@gmail.com
+# Date:         6/10/2021
+# Description:  Build a mathematical challenging puzzle - Petals Around The Rose.
+#   I hereby declare that I completed this work without any improper help
+#   from a third party and without using any aids other than those cited.
+
 import dice
 dice.display_dice(2,3,4,5,6)
+
+print(
+"""
+Petals Around the Rose
+----------------------
+The name of the game is 'Petals Around the Rose'. The name of the
+game is important. The computer will roll five dice and ask you to
+guess the score for the roll. The score will always be zero or an
+even number. Your mission, should you choose to accept it, is to
+work out how the computer calculates the score. If you succeed in
+working out the secret and guess correctly four times in a row, you
+become a Potentate of the Rose.
+"""
+)
