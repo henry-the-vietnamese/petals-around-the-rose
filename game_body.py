8c15adb6b3e3c5cc8315fde6ebda153d44f2f5f4
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-10-09 23:25:12 +1030

    add module docstring

--- game_body.py
+++ game_body.py
@@ -9,6 +9,12 @@
 #   I hereby declare that I completed this work without any improper help
 #   from a third party and without using any aids other than those cited.
 
+"""
+The random module generates 'pseudo-random' numbers - face values on the dice.
+The predefined dice module displays the face value of dice to the screen.
+Generally, the random module gives the face of each die a value which is displayed to the screen using the dice module.
+"""
+
 import random
 import dice
 
