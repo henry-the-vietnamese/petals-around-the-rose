435d7e42fe431300a22b028bf6d650ff74594fda
Tan Duc MAI <henry-the-vietnamese@gmail.com>
2021-12-03 20:32:20 +1030

    Add hashtag comment

--- petals.py
+++ petals.py
@@ -9,6 +9,7 @@
 #               Petals Around the Rose.
 #   I hereby declare that I completed this work without any improper help
 #   from a third party and without using any aids other than those cited.
+#
 
 
 # ------------------------------- Module Imports -------------------------------
